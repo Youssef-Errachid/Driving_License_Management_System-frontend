d6acd42917c56ea5703df1e00e21dc18060fb8a1
Youssef <yousseferrachid@gmail.com>
2026-08-25 16:16:34 +0100

    initialize React project with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-center">
+        <h1 className="text-5xl text-blue-950 mt-3 ">
+          Driving License Management System
+        </h1>
+        <p className="mt-3  text-gray-600">
+          DLMS is a modern Driving License Management System designed to
+          simplify and streamline the management of driving licenses, drivers,
+          applications, and related administrative processes through a secure
+          and user-friendly platform.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
