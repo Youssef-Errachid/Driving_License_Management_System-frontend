c3b70c61f51dfd5f3dd58175572edcc07ba93a36
Youssef <yousseferrachid@gmail.com>
2026-09-23 19:51:57 +0000

    before restructure

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://dlms-backend-production.up.railway.app/api",
+    //baseURL: "https://dlms-backend-production.up.railway.app/api",
+    baseURL: "http://localhost:8080/api",
+
   headers: {
     "Content-Type": "application/json",
   },
